9220046c2e9dc9b8fd1f93fe2fbb630fb19321d8
Kasia4 <katarzyna.rybacka4@gmail.com>
2018-05-05 18:02:13 +0200

    Added relation expressions

---
+++ TKOM-proj/RelExpression.cpp
@@ -0,0 +1,26 @@
+#include "RelExpression.h"
+#include <unordered_map>
+#include <functional>
+
+bool RelExpression::calculate() const
+{
+	auto leftValue = l_operand->calculate();
+	auto rightValue = r_operand->calculate();
+	return calculate(leftValue, rightValue);
+}
+
+bool RelExpression::calculate(int leftValue, int rightValue) const
+{
+	static const std::unordered_map <TokenType, std::function<bool(int,int)> > rel_operations = {
+		{ TokenType::Greater,		[](int l, int r) { return l > r; } },
+		{ TokenType::GreaterOrEq,   [](int l, int r) { return l >= r; } },
+		{ TokenType::Less,			[](int l, int r) { return l < r; } },
+		{ TokenType::LessOrEq,		[](int l, int r) { return l <= r; } },
+		{ TokenType::Equal,			[](int l, int r) { return l == r; } },
+		{ TokenType::NotEqual,		[](int l, int r) { return l != r; } }
+	};
+	auto operation = rel_operations.find(rel_operator);
+	if (operation != rel_operations.end())
+		return operation->second(leftValue, rightValue);
+	throw std::runtime_error("Wrong operator");
+}
---
+++ TKOM-proj/RelExpression.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <memory>
+#include "BoolExpression.h"
+#include "ArithmExpression.h"
+#include "Token.h"
+class RelExpression :
+	public BoolExpression
+{
+	using TokenType = Token::TokenType;
+	using ArithmExpressionPtr = std::unique_ptr<ArithmExpression>;
+public:
+	RelExpression() = default;
+	RelExpression(ArithmExpressionPtr l_operand, ArithmExpressionPtr r_operand, TokenType rel_operator) :
+		l_operand(std::move(l_operand)),
+		r_operand(std::move(r_operand)),
+		rel_operator(rel_operator)
+	{};
+	~RelExpression() = default;
+	bool calculate() const override;
+private:
+	bool calculate(int leftValue, int rightValue) const;
+	ArithmExpressionPtr l_operand;
+	ArithmExpressionPtr r_operand;
+	TokenType rel_operator;
+};
+
